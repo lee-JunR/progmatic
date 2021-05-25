26d01943c72ab1e8a6df49f5fc94913e81505004
lee-JunR <wns2610@icloud.com>
2021-05-25 20:09:17 +0900

    5/25

--- profileapp/decorators.py
+++ profileapp/decorators.py
@@ -1,13 +1,14 @@
 # 코드 간소화를 위한 데코레이터 부분입니다 for views.py
+from profileapp.models import Profile
 
 from django.contrib.auth.models import User
 from django.http import HttpResponseForbidden
 
 #    self = AccountUpdateView, self.get_object()는 기존 data table의 pk값, request.user는 현재 요청을 보내는 유저의 pk 값
-def account_ownership_required(func):
+def profile_ownership_required(func):
     def decorated(request, *args, **kwargs):
-        user = User.objects.get(pk = kwargs['pk'])
-        if not (user) == request.user:
+        profile = Profile.objects.get(pk = kwargs['pk'])
+        if not profile.user == request.user:
             return HttpResponseForbidden()
         return func(request, *args, **kwargs)
     return decorated
