38acaec76d37c86d984edf3cbc4275129d20b045
lee-JunR <wns2610@icloud.com>
2021-07-06 14:20:26 +0900

    ALLOWED_HOSTS = ['0,0,0,0'] 추가

--- pragmatic/settings.py
+++ pragmatic/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = 'django-insecure-7$&6$vot&t%ihq35fx$7ap@#wlc0m%@j&%luw@$ll(khwpt%*%
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['0.0.0.0']
 
 
 # Application definition
